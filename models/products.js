ce63fea2a8a4c441aa2f5febbfee0150320f6576
jpdenford <jpdenford@gmail.com>
2016-05-11 15:08:48 +1200

    products route connects to db

--- models/products.js
+++ models/products.js
@@ -1,13 +1,35 @@
-//var dbClient = require('./db')
+var dbClient = require('./db')
 
 exports.get = function(id, cb){
-	//TODO db query goes here
-	cb(null,exampleProduct);
+	var query = dbClient.query("select * from products where pid = $1",[id]);
+	var data;
+	query.on('row',function(d){
+		data = d;
+	});
+
+	query.on('error',function(er){
+		cb(er,null);
+	});
+
+	query.on('end',function(e){
+		cb(null,data);
+	})
 }
 
 exports.getAll = function(cb){
-	//TODO db query goes here
-	cb(null,[exampleProduct,exampleProduct,exampleProduct,exampleProduct]);
+	var query = dbClient.query("select * from products");
+	var data = [];
+	query.on('row',function(d){
+		data.push(d);
+	});
+
+	query.on('error',function(er){
+		cb(er,null);
+	});
+
+	query.on('end',function(e){
+		cb(null,data);
+	})
 }
 
 //example initial testing / front end
