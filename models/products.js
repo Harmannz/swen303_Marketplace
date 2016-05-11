f18c7bc811a691acf5f76889d7d5f9574716c604
jpdenford <jpdenford@gmail.com>
2016-05-11 18:58:09 +1200

    fixed merge confilct

--- models/products.js
+++ models/products.js
@@ -31,29 +31,3 @@ exports.getAll = function(cb){
 		cb(null,data);
 	})
 }
-
-//example initial testing / front end
-var exampleProduct = {
-		productId: 7000,
-		name: "Test Product",
-		Description: "An amazing thing, the likes of which you have never seen",
-		specification:{
-			color:"pink",
-			rpm: "5700",
-			Mbps: "800"
-		},
-		tags:['tag1','tag2'],
-		category: "Hard Drives",
-		ratingReview: 0.8,
-		price: {
-			value:200,
-			currency:"NZD"
-		},
-		borrowable: true,
-		dimensions: {
-			width:200,
-			height:400,
-			weight:400
-		},
-		quantity: 30,
-	};
