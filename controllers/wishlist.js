d2528a4d8d542424a91d1704801f81e4f5235e94
jpdenford <jpdenford@gmail.com>
2016-05-12 00:42:37 +1200

    Added wishlist route

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -6,36 +6,38 @@ router.get('/:userId', function(req,res){
 	var userId = req.params.userId;
 	console.log("GET /wishlist/" + userId);
 
-	Wishlist.get(userId,function(err, products){
-		if(!err){
-			res.json(products);
-		}else{
-			processError(err,res);
-		}
+	Wishlist.get(userId,function(products){
+		res.json(products);
+	},function(err){
+		res.sendStatus(500);
 	})
 })
 
-router.put('/:userId', function(req,res){
+router.post('/:userId/:productId', function(req,res){
 	var userId = req.params.userId;
-	var products = req.body;
+	var productId = req.params.productId;
 	console.log("POST /wishlist/" + userId);
-	console.log(products);
 
-	Wishlist.addProducts(userId, products, function(err){
-		if(!err){
-			res.sendStatus(201);
-		}else{
-			processError(err,res);
-		}
+	Wishlist.addProduct(userId, productId, function(success){
+		if(success) res.sendStatus(201);
+		else res.sendStatus(400);
+	},function(err){
+		console.log(err);
+		res.sendStatus(500);
 	})
 })
 
-function processError(error,res){
-	console.log(error);
-	//TODO analyse error send relevant code
-	//alternatively can pass an error callback function to model
-	res.sendStatus(500); 
-}
+router.delete('/:userId/:productId', function(req,res){
+	var userId = req.params.userId;
+	var productId = req.params.productId;
+	console.log("DELETE /wishlist/" + userId);
 
+	Wishlist.removeProduct(userId, productId, function(){
+		res.sendStatus(200);
+	},function(err){
+		console.log(err);
+		res.sendStatus(500);
+	})
+})
 
 module.exports = router
--- models/wishlist.js
+++ models/wishlist.js
@@ -1,21 +1,39 @@
-//	dbClient = require('./db');
-var _ = require('lodash');
+var db = require('./db')
+   , _ = require('lodash');
 
-exports.get = function(userId,cb){
-	//TODO db query goes here
-	cb(null,exampleWishlist);
+exports.get = function(userId,cb,errorCb){
+	var rows = [];
+	var query = db.query('select products.* from wishlist, users, products where userid = uid and pid = productid and uid = $1',[userId]);
+	query.on('row',function(r){
+		rows.push(r);
+	})
+	query.on('error',function(e){
+		errorCb(e);
+	})
+	query.on('end',function(summary){
+		cb(rows);
+	})
+}	
+
+exports.addProduct = function(userId, productId, cb, errorCb){
+	//insert if doesnt already exist
+	var query = db.query('insert into wishlist (userid,productid) select $1, $2 where not exists ( select * from wishlist where userid = $1 and productid = $2)',[userId,productId]);
+	
+	query.on('error',function(e){
+		errorCb(e);
+	})
+
+	query.on('end',function(summary){
+		cb(summary.rowCount > 0);
+	})
 }
 
-exports.addProducts = function(userId, products, cb){
-	//TODO replace with db call
-	exampleWishlist.productIds = _.union(exampleWishlist.productIds,products);
-
-	cb(null);
+exports.removeProduct = function(userId, productId, cb, errorCb){
+	var query = db.query('delete from wishlist where userid = $1 and productid = $2',[userId,productId]);
+	query.on('error',function(err){
+		errorCb(err);
+	})
+	query.on('end',function(data){
+		cb(data);
+	})
 }
-
-//example data
-
-var exampleWishlist = {
-		userId : 10,
-		productIds: [7000,472,4892]
-	};
