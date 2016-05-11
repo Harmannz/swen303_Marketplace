02b83d43a237f22e5bf05c3e0b03ae1d33bea70f
Johnny D <jpdenford@gmail.com>
2016-05-11 13:43:30 +1200

    Added dummy_data folder with basic users table

---
+++ dummy_data/users.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+	uid serial primary key,
+	username  character varying(50) NOT NULL,
+	firstname character varying(30),
+	lastname  character varying(30),
+	email  character varying(50),
+	dob  DATE,
+	address character varying(100),
+	password character varying(100) NOT NULL
+);
+
+SET datestyle = "ISO, DMY";
+
+--note this site is only a demonstration hence the plaintext password
+COPY users (username, firstname, lastname, email, dob, address, password) FROM stdin;
+abc	Abe	Carry	abeCarry@gmail.com	10/05/1993	123 Merry Lane Chrischurch 1234	password1
+def	Dedrie	Fredricks	DedrieFredricks@hotmail.com	24/08/1993	567 Maddi Lane Chrischurch 5678	password2
+\.
