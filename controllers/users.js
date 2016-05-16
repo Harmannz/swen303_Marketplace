5ee7b3572d9251352874a786d288b5375f7ae7a3
Brock Wills <brock@pricemaker.co.nz>
2016-05-16 15:47:42 +1200

    Users endpoints now deal with passwords

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 var express = require('express')
   , router = express.Router()
   , Users = require('../models/users')
+  , bcrypt = require('bcrypt');
 
 /*Add a new user to the database*/
 router.post('/', function(req,res){
 	console.log('POST api/users')
 	var user = req.body;
-	Users.addUser(user,function(){
-		res.sendStatus(201);
+	Users.addUser(user,function(result){
+        var user = result.rows[0];
+        delete user.password;
+		res.status(201).json(user);
 	},function(err){
 		console.log(err);
 		res.sendStatus(500);
@@ -15,13 +18,20 @@ router.post('/', function(req,res){
 })
 
 /*Get user based on username or email*/
-router.get('/:id', function(req,res){
+router.post('/:id', function(req,res){
+    var password = req.body.password;
 	console.log('GET api/users/:username or email');
 	var identifier = req.params.id;
 	console.log(identifier);
 
 	Users.fromIdentifier(identifier,function(user){
-		res.json(user);
+        var passwordValid = bcrypt.compareSync(password, user.password);
+        if (passwordValid) {
+            delete user.password;
+            res.json(user);
+        } else {
+            res.sendStatus(400);
+        }
 	},function(err){
 		console.log(err);
 		res.sendStatus(500);
--- models/users.js
+++ models/users.js
@@ -1,11 +1,15 @@
 var dbClient = require('./db')
    ,Utils = require('../models/utils')
-   , _ = require('lodash');
+   , _ = require('lodash')
+   ,bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 exports.addUser = function(data,cb,errorCb){
+    data.password = bcrypt.hashSync(data.password, saltRounds);
 	var queryParams = Utils.shallowObjToQuery(data);
 	var query = dbClient.query("insert into users " + queryParams.string +" returning *" , queryParams.args);
-	
+
 	query.on('error',function(e){
 		errorCb(e);
 	})
@@ -19,7 +23,7 @@ exports.fromIdentifier = function(id,cb,errorCb){
 	field = (id.indexOf(id) > -1)? 'username' : 'email';
 	var query = dbClient.query("select * from users where " + field + " = $1",[id]);
 	var data;
-	
+
 	query.on('row',function(d){
 		data = d;
 	});
