fb6db5541cedf23965b82d0022bfa285e6f6f2e1
CallumGill <callum.gill001@gmail.com>
2016-05-21 15:37:05 +1200

    Added database inputs to populate db with more products

--- dummy_data/specifications.sql
+++ dummy_data/specifications.sql
@@ -12,21 +12,57 @@ COPY specifications (
 	name,
 	value)
 FROM stdin;
-1000	1	Screen Size	4-inches
-1001	1	Screen Resolution	640 x 1136 pixels
-1002	1	Multitouch	Yes
-1003	1	OS	iOS 7, upgradable to iOS 9.3
-1004	1	CPU	Dual-core 1.3 GHz Cyclone (ARM v8-based)
-1005	1	GPU	PowerVR G6430 (quad-core graphics)
-1006	1	GPS	Yes
-2000	2	Effective pixels	Approx. 10.1M
-2001	2	Aspect Ratio	3:2
-2002	2	Playback Zoom	1.5x - 10x
-2003	2	Screen Size	2-inches
-2004	2	GPS	Yes
-3000	3	Dimensions	190 x 120 x 11.4 mm
-3001	3	Resolution	600 x 1024 pixels
-3002	3	OS	Android OS, v2.3 (customized)
-4000	4	Processors	1 (2 Cores)
-4001	4	Resolution	2048x1536 pixels
+1000	1	Product Type	Sofa
+1001	1	Brand	Ikea
+1002	1	Made in	USA
+1003	1	Seats	5
+1004	1	Cover Material	Suede
+1005	1	Fold-out Bed	No
+1006	1	Warranty	6 Months
+
+2000	2	Product Type	Double Bed
+2001	2	Brand	Super Beds
+2002	2	Springs	Firm
+2003	2	Made in	Italy
+2004	2	Warranty	150 Months
+
+3000	3	Product Type	Trundle Bed
+3001	3	Brand	Sealy
+3002	3	Made in	New Zealand
+3003	3	Springs	Soft
+3004	3	Size	King Single
+3005	3	Warranty	120 Months
+
+4000	4	Product Type	Ensemble
+4001	4	Brand	Sealy Posturepedic
+4002	4	Made in	New Zealand
+4003	4	Springs	Firm
+4013	4	Mattress Type	SRx Titanium support spring
+4004	4	Size	Queen
+4005	4	Warranty	60 Months
+
+5000	5	Product Type	Ensemble
+5001	5	Brand	Sleepmaker
+5002	5	Made in	New Zealand
+5003	5	Springs	Medium
+5004	5	Size	Long Single
+5005	5	Warranty	36 Months
+
+6000	6	Product Type	Ensemble
+6001	6	Brand	King Koil
+6002	6	Made in	New Zealand
+6003	6	Springs	Medium
+6013	6	Mattress Material	Purotex, latex, wool, silk
+6004	6	Size	Super King
+6005	6	Warranty	120 Months
+
+7000	7	Product Type	Ensemble
+7001	7	Brand	Sealy
+7002	7	Made in	New Zealand
+7012	7	Mattress Type	Spring
+7013	7	Mattress Thickness	21cm
+7003	7	Springs	Firm
+7004	7	Size	Single
+7005	7	Warranty	120 Months
+
 \.
